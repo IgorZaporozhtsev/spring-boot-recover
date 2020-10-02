16ca04586189386301a350bddd9f2a572b476f6b
zeecoder <igorzaporogtsev@gmail.com>
2020-10-02 07:57:43 +0300

    add ModelMapper and implements update method

--- src/main/java/com/zeecoder/reboot/service/AccountServiceImpl.java
+++ src/main/java/com/zeecoder/reboot/service/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 
 package com.zeecoder.reboot.service;
 
+import com.zeecoder.reboot.config.AccountMapper;
+import com.zeecoder.reboot.dto.AccountDto;
 import com.zeecoder.reboot.model.Account;
 import com.zeecoder.reboot.model.Role;
 import com.zeecoder.reboot.repository.AccountRepository;
@@ -16,12 +18,15 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
+    private final AccountMapper mapper;
+
 
     @Autowired
-    public AccountServiceImpl(@Lazy PasswordEncoder passwordEncoder, AccountRepository repository, RoleService roleService) {
+    public AccountServiceImpl(@Lazy PasswordEncoder passwordEncoder, AccountRepository repository, RoleService roleService, AccountMapper accountMapper) {
         this.passwordEncoder = passwordEncoder;
         this.accountRepository = repository;
         this.roleService = roleService;
+        this.mapper = accountMapper;
     }
 
     @Override
@@ -75,8 +80,29 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public void update(Account account) {
-        accountRepository.save(account);
+    public void update(AccountDto dto) {
+
+        Set<Role> roles = dto.getRoles();
+        Set<Role> addRoles = new HashSet<>();
+        List<Role> allRoles = roleService.getAll();
+
+        for (Role role1 : roles) {
+            boolean coincidence = false;
+            for (Role role2: allRoles){
+                if (role1.getRoleName().equals(role2.getRoleName())){
+                    addRoles.add(role2);
+                    coincidence = true;
+                }
+            }
+            if (!coincidence){
+                addRoles.add(role1);
+            }
+        }
+
+        roles = addRoles;
+        dto.setRoles(roles);
+
+        accountRepository.save(mapper.toEntity(dto));
     }
 
     @Override
