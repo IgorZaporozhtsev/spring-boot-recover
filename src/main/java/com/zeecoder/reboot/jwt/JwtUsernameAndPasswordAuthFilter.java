bcb4f94a65bce1bc33d1d1e343478dc8d44c2b37
zeecoder <igorzaporogtsev@gmail.com>
2020-10-14 10:47:41 +0300

    add new SpringJWT branch

--- src/main/java/com/zeecoder/reboot/jwt/JwtUsernameAndPasswordAuthFilter.java
+++ src/main/java/com/zeecoder/reboot/jwt/JwtUsernameAndPasswordAuthFilter.java
@@ -1,4 +1,73 @@
 package com.zeecoder.reboot.jwt;
 
-public class JwtUsernameAndPasswordAuthFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * the class verified credentials from client
+ *
+ * */
+
+public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+                .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUsername(),
+                authenticationRequest.getPassword());
+
+            Authentication authenticate = authenticationManager.authenticate(authentication);
+
+            return authenticate;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** create token and send to client */
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        String key = "securesecuresecure";
+        String token = Jwts.builder()
+            .setSubject(authResult.getName())
+            .claim("authorities", authResult.getAuthorities())
+            .setIssuedAt(new Date())
+            .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+            .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+            .compact();
+
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
 }
